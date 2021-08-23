03abe289a9d44ee3af056d01aa1d5db7916b5262
PATRICIAGANG <85884951+PATRICIAGANG@users.noreply.github.com>
2021-08-23 08:58:59 +0530

    Update private.py

--- modules/private.py
+++ modules/private.py
@@ -8,34 +8,39 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, I'm {bn} 🔥⚡
-I Cᴀɴ Pʟᴀʏ Mᴜsɪᴄ Iɴ Yᴏᴜʀ Sᴇxʏ Gʀᴏᴜᴩ Vᴏɪᴄᴇ Cʜᴀᴛ. Dᴇᴠᴇʟᴏᴩᴇᴅ Bʏ [G
-TGBᴏᴛXD](https://t.me/TGBotXD).
-
-
-Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴩ Aɴᴅ Pʟᴀʏ Mᴜsɪᴄ Fʀᴇᴇʟʏ!**
-
-
-
-   ==>COMMNADS ARE<==
-❍ /play  - play song you requested
-❍ /dplay  - play song you requested via deezer
-❍ /splay  - play song you requested via jio saavn
-❍ /playlist - Show now playing list
-❍ /current - Show now playing
-❍ /song  - download songs you want quickly
-❍ /search  - search videos on youtube with details
-❍ /deezer  - download songs you want quickly via deezer
-❍ /saavn  - download songs you want quickly via saavn
-❍ /video  - download videos you want quickly
-=>> *Admins only*
-❍ /player - open music player settings panel
-❍ /pause - pause song play
-❍ /resume - resume song play
-❍ /skip - play next song
-❍ /end - stop music play
-❍ /userbotjoin - invite assistant to your chat
-❍ /admincache - Refresh admin list
-"""
+   f"""**Hello 👋 [{}](tg://user?id={})!,
+I Cᴀɴ Pʟᴀʏ Mᴜsɪᴄ Iɴ Yᴏᴜʀ  Gʀᴏᴜᴩ VC\n. Dᴇᴠᴇʟᴏᴩᴇᴅ Bʏ [TGBᴏᴛXD](https://t.me/TGBOTZXD)\nAᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴩ Aɴᴅ Pʟᴀʏ Mᴜsɪᴄ Fʀᴇᴇʟʏ!**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Cᴏᴍᴍᴀɴᴅs🎒", url="t.me/piroXpower")
+                  ],[
+                    InlineKeyboardButton(
+                        "Sᴜᴩᴩᴏʀᴛ⚠️", url="https://t.me/TGBotXD"
+                    ),
+                    InlineKeyboardButton(
+                        "Cʜᴀɴɴᴇʟ📲", url="https://t.me/TGBotzXD"
+                    )
+                  ],[ 
+                    InlineKeyboardButton(
+                        "➕ Gʀᴏᴜᴩ Mᴇ ᴅᴀʟᴅᴏ➕", url="https://t.me/XD_MUSIXBOT?startgroup=true"
+                    )]
+            ]
         ),
-     
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**Aᴍ Oɴʟɪɴᴇ ✅**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔊Uᴩᴅᴀᴛᴇs", url="https://t.me/patricia_updates")
+                ]
+            ]
+        )
+   )
